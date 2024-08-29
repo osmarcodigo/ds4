f6d70df8a9bf9c66fa72b910b7d9207e1f0e2851
osmarcodigo <osmar.velasquez@utp.ac.pa>
2024-08-28 21:24:55 -0600

    Cambio en mensaje de saludo

---
+++ Laboratorio2/Laboratorio2/Program.cs
@@ -0,0 +1,43 @@
+﻿public void Sumar(int valor1, int valor2)
+{
+    int variableLocal = valor1 + valor2;
+   Console.WriteLine(variableLocal);
+}
+//Console.WriteLine(variableLocal);
+
+
+namespace Laboratorio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            client.FirstName = "Su_Nombre";
+            client.LastName = "Su_Apellido";
+            client.Age = 15;
+            client.Id = 1;
+
+            Console.WriteLine(client.GetFullName());
+
+
+        }
+
+
+
+    }
+    public class Client
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public int LastName { get; set; }
+        public ushort Age{ get; set; }
+
+    public string GetFullName()
+    {
+        return FirstName + " " + LastName;
+    }
+ }
+
+
+}
