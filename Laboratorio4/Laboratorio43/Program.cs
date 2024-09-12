6f33c649cd8f6081e658695905db18e8c59f0edc
osmarcodigo <osmar.velasquez@utp.ac.pa>
2024-09-12 00:29:54 -0600

    Laboratorio4

---
+++ Laboratorio4/Laboratorio43/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int suma, cant, valor, prom;
+        string linea;
+        suma = 0;
+        cant = 0;
+        do
+        {
+            Console.Write("Ingrese un numero (0 para finalizar):   ");
+            linea = Console.ReadLine();
+            valor = int.Parse(linea);
+            if (valor != 0)
+            {
+                suma = suma + valor;
+                cant++;
+            }
+        }while (valor != 0);
+        if (cant != 0) {
+            prom = suma / cant;
+            Console.Write("El promedio de los valores ingresados es:");
+            Console.Write(prom);
+        }
+        else
+        {
+            Console.Write("No se ingresaron valores");
+
+        }
+        Console.WriteLine();
+
+    }
+}
